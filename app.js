ebb829b7a33bc778a7545d7e349fcc24e80f828d
alexust1820 <41970276+alexust1820@users.noreply.github.com>
2021-03-24 19:35:34 +0300

    first commit!

---
+++ app.js
@@ -0,0 +1,24 @@
+let score = 0
+
+makeBlock();
+
+function makeBlock() {
+	document.getElementById('js-game-place').innerHTML = `<div class="block" id="js-block"></div>`
+	document.getElementById('js-block').onclick = function p() {
+		console.log(document.getElementById('js-block').clientWidth)
+		changeLoc();
+	}
+}
+
+function changeLoc() {
+	score += 10
+	document.getElementById('js-block').style.left = ((Math.random()) * 370) + 'px'
+	document.getElementById('js-block').style.top = ((Math.random()) * 370) + 'px'
+	console.log(document.getElementById('js-block').style.left, document.getElementById('js-block').style.top)
+	document.getElementById('score').innerHTML = `Your score: ${score}`
+	console.log(score)
+}
+
+function reload() {
+	document.location.reload();
+}
